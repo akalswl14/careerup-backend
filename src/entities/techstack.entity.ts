12b5e93aabe7de11850023fc302a0c4a63613170
akalswl14 <gnkryd12@gmail.com>
2022-02-25 16:46:54 +0900

    Add JoinTable on Techstack Entity

--- src/entities/techstack.entity.ts
+++ src/entities/techstack.entity.ts
@@ -2,14 +2,21 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
+  ManyToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Language } from './language.entity';
 
 @Entity()
 export class Techstack {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: number;
+
+  @ManyToMany(() => Language)
+  @JoinTable()
+  languages: Language[];
 
   @Column({ length: '255' })
   stackName: string;

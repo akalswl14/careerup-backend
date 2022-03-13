dabd84a1ae5dfc8761534f4eac7eb14df8dce934
akalswl14 <gnkryd12@gmail.com>
2022-03-14 01:26:46 +0900

    Fix unneed services, dto, decorator and guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,6 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import {
-  GithubLoginInputDto,
-  GithubLoginOutputDto,
-} from 'src/dto/github-login-user.dto';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 
@@ -16,39 +12,6 @@ export class AuthService {
     private readonly configService: ConfigService,
     private jwtService: JwtService,
   ) {}
-
-  async githubSignUp(user: GithubLoginInputDto): Promise<GithubLoginOutputDto> {
-    const gitUserId = user.gitUserId.toString();
-    const foundGithub = await this.usersRepository.findOne({
-      where: { gitUserId },
-      select: ['id', 'email', 'username', 'profileUrl'],
-    });
-
-    if (foundGithub) {
-      const { id, username, profileUrl } = foundGithub;
-      return { id, gitUserId, username, profileUrl };
-    }
-
-    const { id, username, profileUrl } = await this.usersRepository.save({
-      gitUserId,
-      email: user.email,
-      profileUrl: user.profileUrl,
-      username: user.username,
-    });
-    return { id, gitUserId, username, profileUrl };
-  }
-
-  async signToken(user) {
-    try {
-      const payload = { sub: user.id };
-
-      return {
-        accessToken: this.jwtService.sign(payload),
-      };
-    } catch (e) {
-      throw new ConflictException('로그인이 불가한 유저입니다.');
-    }
-  }
 
   async githubLogin(req) {
     if (!req.user) throw new ConflictException('유효하지 않은 유저');

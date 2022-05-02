338de3e1bef010ba205dde5f8fee159aab5b1d8d
akalswl14 <gnkryd12@gmail.com>
2022-05-03 04:20:27 +0900

    Fix Typo

--- src/entities/wish-recruit.entity.ts
+++ src/entities/wish-recruit.entity.ts
@@ -7,7 +7,7 @@ import {
 import { Recruit } from './recruit.entity';
 import { User } from './user.entity';
 
-@Entity('WistRecruit')
+@Entity('WishRecruit')
 export class WishRecruit {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: string;

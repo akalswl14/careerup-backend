6cabb3f07c498b360e3eb3be23b623deef38ad0d
akalswl14 <gnkryd12@gmail.com>
2022-05-03 04:22:42 +0900

    Add Recruit module on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { RecruitModule } from './recruit/recruit.module';
 import { TestdataModule } from './testdata/testdata.module';
 import { config } from 'ormconfig';
 import { TaskModule } from './task/task.module';
@@ -18,6 +19,7 @@ import { TaskModule } from './task/task.module';
     TypeOrmModule.forRoot(config),
     UserModule,
     AuthModule,
+    RecruitModule,
     TestdataModule,
     TaskModule,
   ],

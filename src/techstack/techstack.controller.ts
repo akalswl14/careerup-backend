6c1ebfbac72dc5a7806b81f284507f6a2126919e
akalswl14 <gnkryd12@gmail.com>
2022-05-17 03:06:41 +0900

    Add getSearchTask

--- src/techstack/techstack.controller.ts
+++ src/techstack/techstack.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { searchOptionDto } from 'src/dto/output.dto';
+import { TechstackService } from './techstack.service';
 
+@ApiTags('기술스택 API')
 @Controller('techstack')
-export class TechstackController {}
+export class TechstackController {
+  constructor(private readonly techstackService: TechstackService) {}
+
+  @Get('search')
+  @ApiOperation({
+    summary: '검색 Techstack 옵션 목록 API',
+    description: '검색 페이지에서, 선택할 Techstack 목록을 반환한다.',
+  })
+  @ApiOkResponse({
+    description: '희망 직무로 선택한 Techstack 목록을 반환한다. ',
+    type: searchOptionDto,
+    isArray: true,
+  })
+  async getSearchTask(): Promise<searchOptionDto[]> {
+    return this.techstackService.getSearchTechStack();
+  }
+}
